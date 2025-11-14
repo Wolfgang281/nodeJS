499008294e98b5dd45ac19ee2c9eb8708b2efb11
utkarsh <utkarshgupta.281@gmail.com>
2025-11-14 11:59:17 +0530

    add project config

---
+++ Project/backend/app.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
+const app = express();
+
+app.use(express.json()); //? to handle json data
+app.use(express.urlencoded({ extended: true })); //? to handle form data
+
+export default app;
---
+++ Project/backend/src/models/user.model.js
@@ -0,0 +1,37 @@
+//! username, email, password, role, contactNumber
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      default: "user",
+    },
+    contactNumber: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    isVerified: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true, toJSON: "", toObject: "" }
+);
