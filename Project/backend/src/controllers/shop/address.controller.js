c529200d87e90fb6d572c2c49f23c5bdb78bd90a
utkarsh <utkarshgupta.281@gmail.com>
2025-11-18 14:56:11 +0530

    add address model

---
+++ Project/backend/src/controllers/shop/address.controller.js
@@ -0,0 +1,11 @@
+import expressAsyncHandler from "express-async-handler";
+
+export const addAddress = expressAsyncHandler(async (req, res) => {});
+
+export const getAddresses = expressAsyncHandler(async (req, res, next) => {});
+
+export const getAddress = expressAsyncHandler(async (req, res, next) => {});
+
+export const updateAddress = expressAsyncHandler(async (req, res, next) => {});
+
+export const deleteAddress = expressAsyncHandler(async (req, res, next) => {});
